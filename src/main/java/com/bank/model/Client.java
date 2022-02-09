076d827fc97a5900d69599817ae7869b813f73d8
Jose Carlos Mejia Huayhua <jmejiahu@emeal.nttdata.com>
2022-02-09 09:35:04 -0500

    cambios

--- src/main/java/com/bank/model/Client.java
+++ src/main/java/com/bank/model/Client.java
@@ -3,11 +3,15 @@ package com.bank.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Document
 public class Client {
 	@Id
